045c1f9ceb08261023341e487ced787d50a0f078
Medet Mussayev <acostyle.m.z@protonmail.com>
2021-08-27 18:30:29 +0600

    Add WYSIWYG to admin panel

--- places/models.py
+++ places/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from django.db.models.base import Model
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     """Место."""
     title = models.CharField("Название места", max_length=200, db_index=True)
     description_short = models.TextField("Краткое описание", null=True, blank=True)
-    description_long = models.TextField("Полное описание", null=True, blank=True)
+    description_long = HTMLField("Полное описание", null=True, blank=True)
     latitude = models.FloatField("Широта", null=True, blank=True)
     longitude = models.FloatField("Долгота", null=True, blank=True)
+    
 
     def __str__(self):
         return f"{self.id} - {self.title}"
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.generate_place_info),
     path('places/<int:place_id>', views.get_place_id, name='get_place_id'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
