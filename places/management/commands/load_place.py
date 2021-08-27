25e5d0c394ae00c4fdf4b22af795a7fae5f01635
Medet Mussayev <acostyle.m.z@protonmail.com>
2021-08-27 20:01:27 +0600

    Add load_place.py

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+from places.models import Image, Place
+import requests
+import os
+
+
+class Command(BaseCommand):
+    help = 'Load a Place from JSON'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json', help='Place JSON URL')
+
+    def handle(self, *args, **options):
+        response = requests.get(options['json'])
+        response.raise_for_status()
+        new_place = response.json()
+
+        place, created = Place.objects.get_or_create(
+            title=new_place['title'],
+            description_short=new_place['description_short'],
+            description_long=new_place['description_long'],
+            latitude=new_place['coordinates']['latitude'],
+            longitude=new_place['coordinates']['longitude'],
+        )
+
+        image_files = os.listdir('media/')
+        for image in image_files:
+            if image.startswith(place.title.replace(' ', '_')):
+                os.remove(f'media/{image}')
+        old_images_in_db = Image.objects.filter(place=place)
+        old_images_in_db.delete()
+
+        for picture_number, image_url in enumerate(new_place['imgs']):
+            response = requests.get(image_url)
+            response.raise_for_status()
+            image = Image.objects.create(place=place)
+            image.image.save(
+                f'{place.title}_{picture_number}.jpg',
+                ContentFile(response.content),
+                save=True
+            )
+        if created:
+            self.stdout.write(f'New place {place.title} has been loaded.')
+        else:
+            self.stdout.write(
+                f'The place {place.title} already exists, all photos have been replaced.'
+            )
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY') or "django-insecure-$q$elb9oneyh-#od99g1(0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'aco4fun.pythonanywhere.com']
 
 
 # Application definition
